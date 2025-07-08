969e95317212ad83b5e4844a7cef464cc1e37be6
Amairi <selsabilamairi@gmail.com>
2025-07-08 15:30:28 +0200

    feat: amélioration du style et navigation

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 // Types de navigation
-import { RootStackParamList } from './src/screens/navigation/types';
+import { RootStackParamList } from './src/navigation/types';
 
 // Écrans
 import HomeScreen from './src/screens/HomeScreen';
