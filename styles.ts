b72d703c8a4e78dadbcecc79f1d444588bd93872
Karim <karim.ayoub.job@gmail.com>
2025-07-08 10:10:51 +0200

    Add simple style and create simple.ts

---
+++ styles.ts
@@ -0,0 +1,24 @@
+import { StyleSheet, StatusBar, Platform } from "react-native";
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "ghostwhite",
+    ...Platform.select({
+      ios: { paddingTop: 20 },
+      android: { paddingTop: StatusBar.currentHeight },
+    }),
+  },
+  box: {
+    width: 100,
+    height: 100,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "lightgray",
+  },
+  boxText: {
+    color: "darkslategray",
+    fontWeight: "bold",
+  },
+});
