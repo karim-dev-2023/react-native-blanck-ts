d3dabd04c516012d81087955403784d2e544073b
Karim <karim.ayoub.job@gmail.com>
2025-07-08 11:14:02 +0200

    Optimisation du code

--- App.tsx
+++ App.tsx
@@ -1,19 +1,15 @@
-import { StatusBar } from "expo-status-bar";
+import React from "react";
 import { Text, View } from "react-native";
 import styles from "./styles";
+import Box from "./components/Box";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style={styles.box}>
-        <Text style={styles.boxText}>#1</Text>
-      </View>
-      <View style={styles.box}>
-        <Text style={styles.boxText}>#2</Text>
-      </View>
-      <View style={styles.box}>
-        <Text style={styles.boxText}>#3</Text>
-      </View>
+      <Box>#1</Box>
+      <Box>#2</Box>
+      <Box>#3</Box>
+      <Box>#4</Box>
     </View>
   );
 }
